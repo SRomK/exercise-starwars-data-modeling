c715a5da29d9119535d3b77754f569733da4cac5
Sofia Romero <75990610+SRomK@users.noreply.github.com>
2022-06-09 15:03:34 +0200

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy import Column, ForeignKey, Integer, String, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +9,66 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+   
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    firstname = Column(VARCHAR(250), nullable=False)
+    lastname = Column(VARCHAR(250), nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+    
 
     def to_dict(self):
-        return {}
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            
+        }
+
+    
+
+class Favorites(Base):
+    __tablename__= 'favorites'   
+
+    id = Column(Integer, primary_key=True)
+    planetfav_name = Column(String(250))
+    characterfav_name = Column(String(250))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "planetfav_name": self.planetfav_name,
+            "characterfav_name": self.characterfav_name
+        }
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    
+    id = Column(Integer, primary_key=True)
+    planets_name = Column(String(250))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "planets_name": self.planets_name
+        }
+    
+class Characters(Base):
+    __tablename__='characters'
+
+    id = Column(Integer, primary_key=True) 
+    characters_name = Column(String(250))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "characters_name": self.characters_name
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
